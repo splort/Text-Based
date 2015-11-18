97baf16b2758b733159c1d4075b502fa81a05383
Andre Golin <dehgolin@gmail.com>
2015-11-18 17:15:00 +0000

    First commit

---
+++ Game.py
@@ -0,0 +1,18 @@
+print ("You wake up with no memory of anything. A gun lies on the table beside you, and bullets are spread about it.")
+tableGun = input ("Do you pick it up or not?").lower()
+if tableGun == ("yes"):
+    print ("The gun is cold in your hand, and very heavy. You, despite your amnesia, expertly load the weapon. Your attention then turns to the dark room around you.")
+else:
+    print ("You stand, ignoring the gun, and turn in a circle, surveying the dark room around you.")
+print ("The room offers little in the way of comfort, but a wardrobe rests conspicuously in the corner, and a door is opposite you.")
+roomQ = input ("Do you go to the door, the wardrobe, or explore further?").lower()
+roomOne = ("wardrobe")
+roomOneA = ("door")
+roomOneB = ("explore")
+if roomOne in roomQ:
+    print ("You approach the wardrobe cautiously. It is ")
+elif
+    print ("")
+
+
+# answer1 = input ("A man approaches you with a gun. His gun is raised, but his hand is shaking. Do you shoot him, try to reason, or turn and run?")
